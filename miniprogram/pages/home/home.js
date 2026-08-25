f9f7416e245fd93d7697b9b4ddf861dfc080c54a
renjun <312827559@qq.com>
2026-08-25 19:18:05 +0800

    design: simplify BatchFrame visual system

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -1,6 +1,11 @@
 Page({
   data: {
-    
+    topInset: 24
+  },
+
+  onLoad() {
+    const info = wx.getWindowInfo ? wx.getWindowInfo() : wx.getSystemInfoSync();
+    this.setData({ topInset: Math.max(24, (info.statusBarHeight || 0) + 20) });
   },
 
   goToFrame() {
@@ -9,4 +14,3 @@ Page({
     });
   }
 });
-
